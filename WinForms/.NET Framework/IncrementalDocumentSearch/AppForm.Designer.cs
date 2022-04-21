60d66e1530affc885bd7a286eb21028d0ae0962f
O2 Solutions Support <techsupport@o2sol.com>
2022-04-21 13:20:45 +0300

    Added IncrementalDocumentSearch sample

---
+++ WinForms/.NET Framework/IncrementalDocumentSearch/AppForm.Designer.cs
@@ -0,0 +1,214 @@
+﻿namespace IncrementalDocumentSearch
+{
+    partial class AppForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtFile = new System.Windows.Forms.TextBox();
+            this.btnBrowse = new System.Windows.Forms.Button();
+            this.pdfDoc = new O2S.Components.PDFView4NET.PDFDocument(this.components);
+            this.pageView = new O2S.Components.PDFView4NET.PDFPageView();
+            this.ofd = new System.Windows.Forms.OpenFileDialog();
+            this.txtSearchText = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnFindNext = new System.Windows.Forms.Button();
+            this.btnStartSearch = new System.Windows.Forms.Button();
+            this.btnFindPrevious = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(3, 10);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(47, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "PDF file:";
+            // 
+            // txtFile
+            // 
+            this.txtFile.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtFile.Location = new System.Drawing.Point(56, 6);
+            this.txtFile.Name = "txtFile";
+            this.txtFile.ReadOnly = true;
+            this.txtFile.Size = new System.Drawing.Size(1276, 20);
+            this.txtFile.TabIndex = 1;
+            // 
+            // btnBrowse
+            // 
+            this.btnBrowse.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnBrowse.Location = new System.Drawing.Point(1338, 6);
+            this.btnBrowse.Name = "btnBrowse";
+            this.btnBrowse.Size = new System.Drawing.Size(25, 20);
+            this.btnBrowse.TabIndex = 2;
+            this.btnBrowse.Text = "...";
+            this.btnBrowse.UseVisualStyleBackColor = true;
+            this.btnBrowse.Click += new System.EventHandler(this.btnBrowse_Click);
+            // 
+            // pdfDoc
+            // 
+            this.pdfDoc.Metadata = null;
+            this.pdfDoc.PageLayout = O2S.Components.PDFView4NET.PDFPageLayout.SinglePage;
+            this.pdfDoc.PageMode = O2S.Components.PDFView4NET.PDFPageMode.UseNone;
+            // 
+            // pageView
+            // 
+            this.pageView.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pageView.AutoScroll = true;
+            this.pageView.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pageView.DefaultEllipseAnnotationBorderWidth = 1D;
+            this.pageView.DefaultInkAnnotationWidth = 1D;
+            this.pageView.DefaultRectangleAnnotationBorderWidth = 1D;
+            this.pageView.Document = this.pdfDoc;
+            this.pageView.DownscaleLargeImages = false;
+            this.pageView.EnableRepeatedKeys = false;
+            this.pageView.GraphicEngine = O2S.Components.PDFView4NET.PDFGraphicEngine.Quadrifoglio;
+            this.pageView.Location = new System.Drawing.Point(6, 59);
+            this.pageView.Name = "pageView";
+            this.pageView.PageNumber = 0;
+            this.pageView.RenderingProgressColor = System.Drawing.Color.Empty;
+            this.pageView.RequiredFormFieldHighlightColor = System.Drawing.Color.Empty;
+            this.pageView.ScrollPosition = new System.Drawing.Point(0, 0);
+            this.pageView.Size = new System.Drawing.Size(1357, 461);
+            this.pageView.SnapRectanglesToGrid = true;
+            this.pageView.SubstituteFonts = null;
+            this.pageView.TabIndex = 4;
+            this.pageView.WorkMode = O2S.Components.PDFView4NET.UserInteractiveWorkMode.PanAndScan;
+            // 
+            // ofd
+            // 
+            this.ofd.Filter = "PDF files (*.pdf)|*.pdf|All files (*.*)|*.*";
+            // 
+            // txtSearchText
+            // 
+            this.txtSearchText.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtSearchText.Location = new System.Drawing.Point(56, 32);
+            this.txtSearchText.Name = "txtSearchText";
+            this.txtSearchText.Size = new System.Drawing.Size(968, 20);
+            this.txtSearchText.TabIndex = 6;
+            this.txtSearchText.Text = "PDF4NET";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(3, 35);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(31, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Text:";
+            // 
+            // btnClear
+            // 
+            this.btnClear.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnClear.Location = new System.Drawing.Point(1288, 30);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(75, 23);
+            this.btnClear.TabIndex = 9;
+            this.btnClear.Text = "Clear search";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // btnFindNext
+            // 
+            this.btnFindNext.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnFindNext.Location = new System.Drawing.Point(1207, 30);
+            this.btnFindNext.Name = "btnFindNext";
+            this.btnFindNext.Size = new System.Drawing.Size(75, 23);
+            this.btnFindNext.TabIndex = 8;
+            this.btnFindNext.Text = "Find next";
+            this.btnFindNext.UseVisualStyleBackColor = true;
+            this.btnFindNext.Click += new System.EventHandler(this.btnFindNext_Click);
+            // 
+            // btnStartSearch
+            // 
+            this.btnStartSearch.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnStartSearch.Location = new System.Drawing.Point(1030, 30);
+            this.btnStartSearch.Name = "btnStartSearch";
+            this.btnStartSearch.Size = new System.Drawing.Size(75, 23);
+            this.btnStartSearch.TabIndex = 7;
+            this.btnStartSearch.Text = "Start search";
+            this.btnStartSearch.UseVisualStyleBackColor = true;
+            this.btnStartSearch.Click += new System.EventHandler(this.btnStartSearch_Click);
+            // 
+            // btnFindPrevious
+            // 
+            this.btnFindPrevious.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnFindPrevious.Location = new System.Drawing.Point(1111, 30);
+            this.btnFindPrevious.Name = "btnFindPrevious";
+            this.btnFindPrevious.Size = new System.Drawing.Size(90, 23);
+            this.btnFindPrevious.TabIndex = 10;
+            this.btnFindPrevious.Text = "Find previous";
+            this.btnFindPrevious.UseVisualStyleBackColor = true;
+            this.btnFindPrevious.Click += new System.EventHandler(this.btnFindPrevious_Click);
+            // 
+            // AppForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1369, 526);
+            this.Controls.Add(this.btnFindPrevious);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnFindNext);
+            this.Controls.Add(this.btnStartSearch);
+            this.Controls.Add(this.txtSearchText);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.pageView);
+            this.Controls.Add(this.btnBrowse);
+            this.Controls.Add(this.txtFile);
+            this.Controls.Add(this.label1);
+            this.Name = "AppForm";
+            this.Text = "PDFView4NET - Incremental document search";
+            this.Load += new System.EventHandler(this.AppForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtFile;
+        private System.Windows.Forms.Button btnBrowse;
+        private O2S.Components.PDFView4NET.PDFDocument pdfDoc;
+        private O2S.Components.PDFView4NET.PDFPageView pageView;
+        private System.Windows.Forms.OpenFileDialog ofd;
+        private System.Windows.Forms.TextBox txtSearchText;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnFindNext;
+        private System.Windows.Forms.Button btnStartSearch;
+        private System.Windows.Forms.Button btnFindPrevious;
+    }
+}
+
---
+++ WinForms/.NET Framework/IncrementalDocumentSearch/AppForm.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using O2S.Components.PDFView4NET.Text;
+
+namespace IncrementalDocumentSearch
+{
+    public partial class AppForm : Form
+    {
+        public AppForm()
+        {
+            InitializeComponent();
+        }
+
+        private string textToSearch = "";
+        private int crtPageNumber = 0;
+        private PDFSearchTextResultCollection strc = null;
+        private int crtSearchResult = 0;
+
+        private void AppForm_Load(object sender, EventArgs e)
+        {
+            txtFile.Text = "multicolumntextandimages.pdf";
+            pdfDoc.FilePath = txtFile.Text;
+        }
+
+        private void btnBrowse_Click(object sender, EventArgs e)
+        {
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                txtFile.Text = ofd.FileName;
+                pdfDoc.FilePath = txtFile.Text;
+            }
+        }
+
+        private void btnStartSearch_Click(object sender, EventArgs e)
+        {
+            pageView.ClearSearch();
+
+            textToSearch = txtSearchText.Text;
+            crtPageNumber = 0;
+            strc = pdfDoc.Pages[crtPageNumber].SearchText(textToSearch);
+            while ((strc == null) || (strc.Count == 0))
+            {
+                crtPageNumber++;
+                if (crtPageNumber >= pdfDoc.Pages.Count)
+                {
+                    MessageBox.Show("Document does not contain specified text.");
+                    strc = null;
+                    break;
+                }
+                strc = pdfDoc.Pages[crtPageNumber].SearchText(textToSearch);
+            }
+            crtSearchResult = 0;
+
+            if (strc != null)
+            {
+                pageView.PageNumber = crtPageNumber;
+                pageView.HighlightSearchResult(strc[crtSearchResult]);
+            }
+        }
+
+        private void btnFindNext_Click(object sender, EventArgs e)
+        {
+            pageView.ClearSearch();
+
+            if (strc != null)
+            {
+                crtSearchResult++;
+                if (crtSearchResult >= strc.Count)
+                {
+                    crtPageNumber++;
+                    if (crtPageNumber < pdfDoc.Pages.Count)
+                    {
+                        strc = pdfDoc.Pages[crtPageNumber].SearchText(textToSearch);
+                        while ((strc == null) || (strc.Count == 0))
+                        {
+                            crtPageNumber++;
+                            if (crtPageNumber >= pdfDoc.Pages.Count)
+                            {
+                                MessageBox.Show("Search has reached the end of document.");
+                                strc = null;
+                                break;
+                            }
+                            strc = pdfDoc.Pages[crtPageNumber].SearchText(textToSearch);
+                        }
+                    }
+                    else
+                    {
+                        MessageBox.Show("Search has reached the end of document.");
+                        strc = null;
+                    }
+                    if (crtPageNumber < pdfDoc.Pages.Count)
+                    {
+                        pageView.PageNumber = crtPageNumber;
+                    }
+                    crtSearchResult = 0;
+                }
+
+                if (strc != null)
+                {
+                    pageView.HighlightSearchResult(strc[crtSearchResult]);
+                }
+            }
+        }
+
+        private void btnFindPrevious_Click(object sender, EventArgs e)
+        {
+            pageView.ClearSearch();
+
+            if (strc != null)
+            {
+                crtSearchResult--;
+                if (crtSearchResult < 0)
+                {
+                    crtPageNumber--;
+                    if (crtPageNumber >= 0)
+                    {
+                        strc = pdfDoc.Pages[crtPageNumber].SearchText(textToSearch);
+                        while ((strc == null) || (strc.Count == 0))
+                        {
+                            crtPageNumber--;
+                            if (crtPageNumber < 0)
+                            {
+                                MessageBox.Show("Search has reached the start of document.");
+                                strc = null;
+                                break;
+                            }
+                            strc = pdfDoc.Pages[crtPageNumber].SearchText(textToSearch);
+                        }
+                    }
+                    else
+                    {
+                        MessageBox.Show("Search has reached the start of document.");
+                        strc = null;
+                    }
+                    if (crtPageNumber >= 0)
+                    {
+                        pageView.PageNumber = crtPageNumber;
+                    }
+                    if (strc != null)
+                    {
+                        crtSearchResult = strc.Count - 1;
+                    }
+                }
+
+                if (strc != null)
+                {
+                    pageView.HighlightSearchResult(strc[crtSearchResult]);
+                }
+            }
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            pageView.ClearSearch();
+            strc = null;
+            crtPageNumber = 0;
+            crtSearchResult = 0;
+        }
+    }
+}
